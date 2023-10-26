68adebeb51cd7c7e2494479eac6f9769a578c328
SebastianRodCimas <56884033+SebastianRodCimas@users.noreply.github.com>
2023-10-26 12:06:02 +0200

    authentification good

--- lib/auth/repository.dart
+++ lib/auth/repository.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,17 +10,27 @@ void setToken(String body) {
 }
 
 Future<String> authenticate(String email, String pwd) async {
-  final response = await http.post(
-    Uri.parse('https://localhost:5220/api/Authentications/Authenticate'),
-    body: jsonEncode(<String, String>{
-      'login': email,
-      'password': pwd,
-    }),
-  );
-  if (response.statusCode == 200) {
-    setToken(response.body);
-    return response.body;
-  } else {
-    throw Exception('Failed to authenticate user.');
+  try {
+    final response = await http.post(
+      Uri.parse('https://localhost:5220/api/Authentications/Authenticate'),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: jsonEncode({
+        'login': email,
+        'password': pwd,
+      }),
+    );
+    print(response);
+    if (response.statusCode == 200) {
+      setToken(response.body);
+      return response.body;
+    } else {
+      throw Exception('Failed to authenticate user.');
+    }
+  } catch (e) {
+    print(e);
+    throw (Exception(e));
   }
 }
